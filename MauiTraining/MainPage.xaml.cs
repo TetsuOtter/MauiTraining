4bc34321b1b6d19d84dc9f4de220b7796ffd1406
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-09 11:44:34 +0900

    過去のカウントを記録するようにした

--- MauiTraining/MainPage.xaml.cs
+++ MauiTraining/MainPage.xaml.cs
@@ -3,10 +3,19 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, "Counter.txt");
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+		if (File.Exists(targetFile))
+		{
+			string lastCount = File.ReadAllText(targetFile);
+
+			if (int.TryParse(lastCount, out int v))
+				count = v;
+		}
 	}
 
 	private async void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +28,8 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		await File.WriteAllTextAsync(targetFile, count.ToString());
 	}
 }
 
